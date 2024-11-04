d055fb634aae121e57b4cdd8d61518875fb07a88
Karin Karlsson <karin.drake@gmail.com>
2024-11-04 15:25:17 +0100

    Header boilerplate

---
+++ src/app/Components/Header.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import styles from "./header.module.css";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <nav>
+        <ul className={styles.links}>
+          <li>
+            <Link href="/gameboy" className={styles.link}>Gameboy</Link>
+          </li>
+          <li>
+            <Link href="/about" className={styles.link}>About</Link>
+          </li>
+        </ul>
+      </nav>
+
+    </header>
+  )
+}
+export default Header
