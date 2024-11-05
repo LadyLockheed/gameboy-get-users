d260437099e65f656eeac58add6bbd5e775dc211
Karin Karlsson <karin.drake@gmail.com>
2024-11-05 16:47:28 +0100

    Implement new components in gameboy

--- src/app/gameboy/page.tsx
+++ src/app/gameboy/page.tsx
@@ -1,12 +1,23 @@
 import styles from './gameboy.module.css'
 import OffScreen  from '../Components/OffScreen'
+import GameButtons from '../Components/GameButtons'
+import SelectAndStartButtons from '../Components/SelectAndStartButtons'
+import DirectionalPad from '../Components/DirectionalPad'
+import SoundOutputBars from '../Components/SoundOutputBars'
 
 const Gameboy = () => {
   return (
     <div className={styles.gameboyContainer}>
       <div className={styles.shell}>
-      <OffScreen/>
+        <OffScreen/>
+        <div className={styles.buttonRow}>
+          <DirectionalPad/>
+          <GameButtons/>
+        </div>
+          <SelectAndStartButtons/>
+          <SoundOutputBars/>
       </div>
+    
     </div>
   )
 }
