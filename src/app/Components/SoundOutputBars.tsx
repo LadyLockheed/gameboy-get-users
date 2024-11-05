c65fcf8537de35c807bc8e20ecba02e7cb05d2c4
Karin Karlsson <karin.drake@gmail.com>
2024-11-05 16:46:45 +0100

    Create soundOutputBars component

---
+++ src/app/Components/SoundOutputBars.tsx
@@ -0,0 +1,16 @@
+import styles from ".//soundOutputBars.module.css";
+
+const SoundOutputBars = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.bar}></div>
+      <div className={styles.bar}></div>
+      <div className={styles.bar}></div>
+      <div className={styles.bar}></div>
+      <div className={styles.bar}></div>
+      <div className={styles.bar}></div>
+    </div>
+  );
+};
+
+export default SoundOutputBars;
