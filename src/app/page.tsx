4adf79a37ca25f90fe5f975ac599fa5be81dd660
Karin Karlsson <karin.drake@gmail.com>
2024-11-05 10:49:43 +0100

    Placeholder text home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 // import Image from "next/image";
-import styles from "./page.module.css";
+// import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-  
-    
-    </div>
+    <h1>Welcome to this exciting game of <span>Get your user</span></h1>
+
   );
 }
