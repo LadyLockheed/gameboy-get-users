c74441ac970a6ddcfad48aee684a57e493958e7a
Karin Karlsson <karin.drake@gmail.com>
2024-11-06 20:37:47 +0100

    Add content and style in About page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,20 @@
+import styles from './about.module.css'
+
 const About = () => {
   return (
-    <div>About</div>
+    <div className={styles.aboutContainer}>
+      <h3>A little bit about me and the project</h3>
+        <p>
+          This is a part of a coding test. 
+          It is built with React, Next.js and TypeScript. <br />
+          My main focus has been to become more familiar with NextJS which is a new framework for me. <br />
+          I have enjoyed the process of creating something inspired by a childhood-memory - specifically, a classic Gameboy<br /> 
+         <br /> 
+
+          The biggest challenge I faced was to stop fiddling with design and styling. I had so much fun that it was difficult to scope my vision and prioritize what was most important to include in the project. 
+          
+        </p>
+      </div>
   )
 }
 export default About
