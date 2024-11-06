90017b30bc87143dff12ab52d9ba0a4146f51345
Karin Karlsson <karin.drake@gmail.com>
2024-11-06 16:42:12 +0100

    Style active link in header

--- src/app/Components/Header.tsx
+++ src/app/Components/Header.tsx
@@ -1,19 +1,24 @@
+'use client'
 import Link from "next/link";
 import styles from "./header.module.css";
+import { usePathname } from "next/navigation";
 
 const Header = () => {
+  
+const currentPath = usePathname()
+
   return (
     <header className={styles.header}>
       <nav>
         <ul className={styles.links}>
         <li>
-            <Link href="/" className={styles.link}>Home</Link>
+            <Link href="/" className={`${styles.link} ${currentPath === '/' && styles.active}`}>Home</Link>
           </li>
           <li>
-            <Link href="/gameboy" className={styles.link}>Gameboy</Link>
+            <Link href="/gameboy" className={`${styles.link} ${currentPath === '/gameboy' && styles.active}`}>Gameboy</Link>
           </li>
           <li>
-            <Link href="/about" className={styles.link}>About</Link>
+            <Link href="/about" className={`${styles.link} ${currentPath === '/about' && styles.active}`}>About</Link>
           </li>
         </ul>
       </nav>
