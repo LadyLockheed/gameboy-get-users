578a5199f66d1589589c43db44b3663b146c86b8
Karin Karlsson <karin.drake@gmail.com>
2024-11-06 21:36:06 +0100

    Improve styling and content of home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-// import Image from "next/image";
-// import styles from "./page.module.css";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <h1>Welcome to this exciting game of <span>Get your user</span></h1>
+    <div className={styles.introText}>
+    <h1>Welcome to my small gameboy-project</h1>
+    <h2>This is a tiny app, built with React, Next.js and TypeScript</h2>
+    <p>It gets data from a free api and display it. This can be seen in the Gameboy page</p>
+    </div>
 
   );
 }
