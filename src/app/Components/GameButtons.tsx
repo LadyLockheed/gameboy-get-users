d084a69eb524d846116af88869ba6776424f208c
Karin Karlsson <karin.drake@gmail.com>
2024-11-06 17:03:36 +0100

    Handle props, create interface

--- src/app/Components/GameButtons.tsx
+++ src/app/Components/GameButtons.tsx
@@ -1,13 +1,22 @@
 import styles from './gameButtons.module.css'
 
-const GameButtons = () => {
+
+interface GameButtonsProps {
+  onClick: () => void;
+  isLoading: boolean;
+  isOn: boolean;
+}
+
+const GameButtons = (props: GameButtonsProps) => {
+
+  const {onClick, isLoading, isOn} = props;
   return (
     <>
     <div className={styles.gameButtonsContainer}>
     <button
       className={styles.gameButtons}
-      // onClick={() => onClick()}
-      // disabled={isLoading}
+      onClick={() => onClick()}
+      disabled={isLoading || !isOn}
     ></button>
     <h5 className={styles.getUserText}>GET USER</h5>
   </div>
