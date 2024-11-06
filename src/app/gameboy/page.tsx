3a5b9e6f1fa70898b62ed8a3b9a309cd6e2697a1
Karin Karlsson <karin.drake@gmail.com>
2024-11-06 17:02:50 +0100

    Fetch data, provide props to children

--- src/app/gameboy/page.tsx
+++ src/app/gameboy/page.tsx
@@ -1,20 +1,55 @@
+'use client'
 import styles from './gameboy.module.css'
 import OffScreen  from '../Components/OffScreen'
 import GameButtons from '../Components/GameButtons'
-import SelectAndStartButtons from '../Components/SelectAndStartButtons'
 import DirectionalPad from '../Components/DirectionalPad'
 import SoundOutputBars from '../Components/SoundOutputBars'
+import OnOffButtons from '../Components/OnOffButtons'
+import { useEffect, useState } from 'react'
+import axios from "axios";
+import { User } from '../Types/user'
 
 const Gameboy = () => {
+
+  const [users, setUsers] = useState<User[] | []>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [hasError, setHasError] = useState<boolean>(false);
+  const [isOn, setIsOn] = useState<boolean>(false)
+
+  useEffect(()=> {
+    //Empty user array when "turn off" gameboy
+    if(!isOn && users.length !== 0) {
+      setUsers([])
+    }
+
+  },[isOn, users.length])
+
+  const fetchUser = async () => {
+    setIsLoading(true);
+
+    if (hasError) {
+      setHasError(false);
+    }
+
+    try {
+      const response = await axios.get("https://randomuser.me/api/");
+      setUsers(response.data.results);
+    } catch (error) {
+      setHasError(true);
+      console.log(error)
+    } finally {
+      setIsLoading(false);
+    }
+  };
   return (
     <div className={styles.gameboyContainer}>
       <div className={styles.shell}>
-        <OffScreen/>
+        <OffScreen isLoading={isLoading} users={users} hasError={hasError} isOn={isOn}/>
         <div className={styles.buttonRow}>
           <DirectionalPad/>
-          <GameButtons/>
+          <GameButtons onClick={fetchUser} isLoading={isLoading} isOn={isOn}/>
         </div>
-          <SelectAndStartButtons/>
+          <OnOffButtons setIsOn={setIsOn}/>
           <SoundOutputBars/>
       </div>
     
