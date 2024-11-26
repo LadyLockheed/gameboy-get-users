3de841300eb7081e1ed30dc82bf294599c623a14
Karin Karlsson <karin.drake@gmail.com>
2024-11-26 17:54:21 +0100

    Remove code that is commented out

--- src/app/Components/Screen.tsx
+++ src/app/Components/Screen.tsx
@@ -16,57 +16,6 @@ const Screen = (props: ScreenProps) => {
 		isOn ? styles.isOn : styles.isOff
 	} ${user ? styles.scrollbar : ''}`;
 
-	// const userData = users?.map((u, index) => {
-	// 	return (
-	// 		<div key={index}>
-	// 			<p>
-	// 				<span className={styles.bold}>Title: </span>
-	// 				{u.name.title}
-	// 			</p>
-	// 			<p>
-	// 				<span className={styles.bold}>Firstname: </span>
-	// 				{u.name.first}
-	// 			</p>
-	// 			<p>
-	// 				<span className={styles.bold}>Lastname: </span>
-	// 				{u.name.last}
-	// 			</p>
-	// 			<p>
-	// 				<span className={styles.bold}>Gender: </span>
-	// 				{u.gender}
-	// 			</p>
-	// 			<p>
-	// 				<span className={styles.bold}>Street: </span>
-	// 				{u.location.street.name}
-	// 			</p>
-	// 			<p>
-	// 				<span className={styles.bold}>City: </span>
-	// 				{u.location.city}
-	// 			</p>
-	// 			<p>
-	// 				<span className={styles.bold}>Country: </span>
-	// 				{u.location.country}
-	// 			</p>
-	// 			<p>
-	// 				<span className={styles.bold}>Phone: </span>
-	// 				{u.phone}
-	// 			</p>
-	// 			<p>
-	// 				<span className={styles.bold}>Email: </span>
-	// 				{u.email}
-	// 			</p>
-	// 			<p>
-	// 				<span className={styles.bold}>Cell: </span>
-	// 				{u.cell}
-	// 			</p>
-	// 			<p>
-	// 				<span className={styles.bold}>Nationality: </span>
-	// 				{u.nat}
-	// 			</p>
-	// 		</div>
-	// 	);
-	// });
-
 	return (
 		<div className={styles.outerFrame}>
 			<div className={screenClassName}>
