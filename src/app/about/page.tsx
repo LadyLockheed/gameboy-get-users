22e70d51442f54af8b5462b4b14bb67eb33ede17
Karin Karlsson <karin.drake@gmail.com>
2024-11-26 17:35:49 +0100

    Refactor text in about page to better fit my portfolio

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,13 +5,15 @@ const About = () => {
 		<div className={styles.aboutContainer}>
 			<h3>A little bit about me and the project</h3>
 			<p>
-				This is a part of a coding test. It is built with React, Next.js and
-				TypeScript. <br />
+				This was originally a part of a coding test. I have refactored it to be
+				a general part of my portfolie.
+				<br />
+				<br />
+				It is built with React, Next.js and TypeScript. <br />
 				My main focus has been to become more familiar with NextJS which is a
 				new framework for me. <br />
 				I have enjoyed the process of creating something inspired by a
 				childhood-memory - specifically, a classic Gameboy
-				<br />
 				<br />
 				The biggest challenge I faced was to stop fiddling with design and
 				styling. I had so much fun that it was difficult to scope my vision and
