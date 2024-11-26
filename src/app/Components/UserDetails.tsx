077ff5ce8463abffa59910a40eef4e775fe48f86
Karin Karlsson <karin.drake@gmail.com>
2024-11-26 17:40:50 +0100

    Create UserDetail component and refactor UserDetails to use UserDetail

--- src/app/Components/UserDetails.tsx
+++ src/app/Components/UserDetails.tsx
@@ -1,5 +1,5 @@
 import { User } from '../types/user';
-import styles from './userDetails.module.css';
+import UserDetail from './UserDetail';
 
 interface UserDetailsProps {
 	user: User;
@@ -8,52 +8,20 @@ const UserDetails = (props: UserDetailsProps) => {
 	const { user } = props;
 
 	const { name, gender, location, phone, email, cell, nat } = user;
+
 	return (
 		<div>
-			<p>
-				<span className={styles.boldText}>Title: </span>
-				{name.title}
-			</p>
-			<p>
-				<span className={styles.boldText}>Firstname: </span>
-				{name.first}
-			</p>
-			<p>
-				<span className={styles.boldText}>Lastname: </span>
-				{name.last}
-			</p>
-			<p>
-				<span className={styles.boldText}>Gender: </span>
-				{gender}
-			</p>
-			<p>
-				<span className={styles.boldText}>Street: </span>
-				{location.street.name}
-			</p>
-			<p>
-				<span className={styles.boldText}>City: </span>
-				{location.city}
-			</p>
-			<p>
-				<span className={styles.boldText}>Country: </span>
-				{location.country}
-			</p>
-			<p>
-				<span className={styles.boldText}>Phone: </span>
-				{phone}
-			</p>
-			<p>
-				<span className={styles.boldText}>Email: </span>
-				{email}
-			</p>
-			<p>
-				<span className={styles.boldText}>Cell: </span>
-				{cell}
-			</p>
-			<p>
-				<span className={styles.boldText}>Nationality: </span>
-				{nat}
-			</p>
+			<UserDetail headline="Title" info={name.title} />
+			<UserDetail headline="Firstname" info={name.first} />
+			<UserDetail headline="LastName" info={name.last} />
+			<UserDetail headline="Gender" info={gender} />
+			<UserDetail headline="Street" info={location.street.name} />
+			<UserDetail headline="City" info={location.city} />
+			<UserDetail headline="Country" info={location.country} />
+			<UserDetail headline="Phone" info={phone} />
+			<UserDetail headline="Email" info={email} />
+			<UserDetail headline="Cell" info={cell} />
+			<UserDetail headline="Nationality" info={nat} />
 		</div>
 	);
 };
