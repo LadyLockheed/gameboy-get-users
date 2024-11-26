21e36e82b38f7caf78398f98809ae9e484d39aa8
Karin Karlsson <karin.drake@gmail.com>
2024-11-26 17:45:25 +0100

    Refactor UserDetails to avoid repetition

--- src/app/Components/UserDetails.tsx
+++ src/app/Components/UserDetails.tsx
@@ -7,21 +7,25 @@ interface UserDetailsProps {
 const UserDetails = (props: UserDetailsProps) => {
 	const { user } = props;
 
-	const { name, gender, location, phone, email, cell, nat } = user;
+	const details = [
+		{ headline: 'Title', info: user.name.title },
+		{ headline: 'Firstname', info: user.name.first },
+		{ headline: 'Lastname', info: user.name.last },
+		{ headline: 'Gender', info: user.gender },
+		{ headline: 'Street', info: user.location.street.name },
+		{ headline: 'City', info: user.location.city },
+		{ headline: 'Country', info: user.location.country },
+		{ headline: 'Phone', info: user.phone },
+		{ headline: 'Email', info: user.email },
+		{ headline: 'Cell', info: user.cell },
+		{ headline: 'Nationality', info: user.nat },
+	];
 
 	return (
 		<div>
-			<UserDetail headline="Title" info={name.title} />
-			<UserDetail headline="Firstname" info={name.first} />
-			<UserDetail headline="LastName" info={name.last} />
-			<UserDetail headline="Gender" info={gender} />
-			<UserDetail headline="Street" info={location.street.name} />
-			<UserDetail headline="City" info={location.city} />
-			<UserDetail headline="Country" info={location.country} />
-			<UserDetail headline="Phone" info={phone} />
-			<UserDetail headline="Email" info={email} />
-			<UserDetail headline="Cell" info={cell} />
-			<UserDetail headline="Nationality" info={nat} />
+			{details.map((detail, index) => (
+				<UserDetail key={index} headline={detail.headline} info={detail.info} />
+			))}
 		</div>
 	);
 };
